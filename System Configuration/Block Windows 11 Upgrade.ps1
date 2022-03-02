189779fd6a255394a119425221207a55b283776e
Josh McBride <45087983+texomans@users.noreply.github.com>
2022-03-01 21:35:43 -0600

    Update Block Windows 11 Upgrade.ps1

--- System Configuration/Block Windows 11 Upgrade.ps1
+++ System Configuration/Block Windows 11 Upgrade.ps1
@@ -1,3 +1,4 @@
+## Run as SYSTEM
 ## 1 Variable is required. Add $TargetVersion as the limit of upgrades. We have a dropdown with both "21H1" and "21H2" depending on the client.
 
 reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate /f /v TargetReleaseVersion /t REG_DWORD /d 1
