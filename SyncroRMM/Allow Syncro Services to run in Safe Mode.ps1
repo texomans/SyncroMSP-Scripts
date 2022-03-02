d2cfad8da876d8fe5daed08bb71f6589b0655743
Josh McBride <45087983+texomans@users.noreply.github.com>
2022-03-01 21:13:14 -0600

    Create Allow Syncro Services to run in Safe Mode.ps1
    This adds the Syncro Services to Safe Mode "Start with Networking" so you can carry on a remote session when troubleshooting in Safe Mode.
    
    This is best added to your onboarding scripts.

---
+++ SyncroRMM/Allow Syncro Services to run in Safe Mode.ps1
@@ -0,0 +1,3 @@
+REG ADD HKLM\SYSTEM\CurrentControlSet\Control\SafeBoot\Network\Syncro /f /ve /t REG_SZ /d Service
+REG ADD HKLM\SYSTEM\CurrentControlSet\Control\SafeBoot\Network\SyncroLive /f /ve /t REG_SZ /d Service
+REG ADD HKLM\SYSTEM\CurrentControlSet\Control\SafeBoot\Network\SyncroOvermind /f /ve /t REG_SZ /d Service
