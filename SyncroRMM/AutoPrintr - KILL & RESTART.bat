5f6d0c87479f847d779c05407ebaa360f0cc462d
Josh McBride <45087983+texomans@users.noreply.github.com>
2022-03-01 21:16:24 -0600

    Delete AutoPrintr - KILL & RESTART.bat

--- SyncroRMM/AutoPrintr - KILL & RESTART.bat
+++
@@ -1,6 +0,0 @@
-net stop "AutoPrintr Service"
-Taskkill /IM AutoPrintr.exe /F
-net start "AutoPrintr Service"
-start "" "C:\Program Files (x86)\RepairShopr\AutoPrintr\AutoPrintr.exe" --new-window/min
-
-exit
