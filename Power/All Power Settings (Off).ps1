d5a9a1ccd076dc8b3e2d7002eabf42b58ef2a4b8
Josh McBride <45087983+texomans@users.noreply.github.com>
2022-03-01 21:13:50 -0600

    Create All Power Settings (Off).ps1
    Turns off disk sleep, PC sleep and hibernate.

---
+++ Power/All Power Settings (Off).ps1
@@ -0,0 +1,11 @@
+powercfg /change monitor-timeout-ac 60
+powercfg /change monitor-timeout-dc 10
+
+powercfg /change disk-timeout-ac 0
+powercfg /change disk-timeout-dc 0
+
+powercfg /change standby-timeout-ac 0
+powercfg /change standby-timeout-dc 0
+
+powercfg /change hibernate-timeout-ac 0
+powercfg /change hibernate-timeout-dc 0
