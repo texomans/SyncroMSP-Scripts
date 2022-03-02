2ffdd7ee3f85eb0e83d6b813f6c9f09855be03a4
Josh McBride <45087983+texomans@users.noreply.github.com>
2022-03-01 21:35:12 -0600

    Update Allow Syncro Services to run in Safe Mode.ps1

--- SyncroRMM/Allow Syncro Services to run in Safe Mode.ps1
+++ SyncroRMM/Allow Syncro Services to run in Safe Mode.ps1
@@ -1,3 +1,5 @@
+## Run as SYSTEM
+
 REG ADD HKLM\SYSTEM\CurrentControlSet\Control\SafeBoot\Network\Syncro /f /ve /t REG_SZ /d Service
 REG ADD HKLM\SYSTEM\CurrentControlSet\Control\SafeBoot\Network\SyncroLive /f /ve /t REG_SZ /d Service
 REG ADD HKLM\SYSTEM\CurrentControlSet\Control\SafeBoot\Network\SyncroOvermind /f /ve /t REG_SZ /d Service
