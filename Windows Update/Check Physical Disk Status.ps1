8890a9caa13e294dcdaebd203200d79aa9482a41
Josh at Texoma Network Solutions <45087983+texomans@users.noreply.github.com>
2022-03-02 16:59:19 -0600

    Create Check Physical Disk Status.ps1

---
+++ Windows Update/Check Physical Disk Status.ps1
@@ -0,0 +1,24 @@
+## Run as SYSTEM
+
+Import-Module $env:SyncroModule
+
+try {
+    $Disks = get-physicaldisk | Where-Object { $_.HealthStatus -ne "Healthy" }
+}
+catch {
+    write-output "Command has Failed: $($_.Exception.Message)"
+    exit 1
+}
+ 
+if ($disks) {
+    write-output "Check Diagnostics. Possible disk failure."
+    write-output $disks
+    exit 1
+}
+else {
+    write-output "Healthy"
+}
+
+if( -not($_.HealthStatus -ne "Healthy")){
+  Rmm-Alert -Category "Physical Disk Failure" -Body "Warning: Check script output. Possible disk failure."
+}
