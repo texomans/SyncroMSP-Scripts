6a886cd7c609e721b0af4c3fb5df92c0d00c0b14
Josh McBride <45087983+texomans@users.noreply.github.com>
2022-03-01 21:56:04 -0600

    Create Uninstall Something (Try to at least).ps1

---
+++ Add-Remove Software/Uninstall Something (Try to at least).ps1
@@ -0,0 +1,35 @@
+## Run as SYSTEM
+## Be sure to add a variable for $uninstallProg. We usually are able to just copy and paste the program name from the list of "Installed Apps" on the asset.
+
+
+Import-Module $env:SyncroModule
+#attempt to uninstall an application via powershell
+$GetApps = Get-WmiObject -Class Win32_Product | Where-Object { $_.Name -Contains $uninstallProg }
+$GetPackages = Get-Package -Provider Programs -IncludeWindowsInstaller -Name "$uninstallProg"
+$GetProducts = wmic product where "name like '%%$uninstallProg%%'"
+
+if ($GetApps -eq $uninstallProg) {
+    Write-Host "Attempting to uninstall $uninstallProg based on WMI info:`n $removeApp"
+    $GetAppsUnInstall.Uninstall()
+}
+elseif ($GetPackages -eq $uninstallProg) {    
+    Write-Host "Attempting to uninstall $uninstallProg based on Get-Package method."
+    Uninstall-Package -Name $uninstallProg
+}
+elseif ($GetProducts -contains $uninstallProg) {
+    Write-Host "Attempting to uninstall $uninstallProg based on WMIC method"
+    wmic product where "name like '%%$uninstallProg%%'" call uninstall /nointeractive
+}
+else {
+    Write-Host "Could not uninstall using above methods. Try using one of the below programs with the Get-Package method."
+    $WMIPrograms = Get-WmiObject -Class Win32_Product
+    $GetPackagesPrograms = Get-Package -Provider Programs -IncludeWindowsInstaller
+    # $WMICPrograms = wmic product list
+    Rmm-Alert -Category 'Failed Uninstall' -Body 'Could not uninstall using automated methods. Check the listed programs file that has been attached to the asset.'
+    $filename = "C:\.texomans\SyncroRMM\Installed Programs.txt"
+    $WMIPrograms | Select-Object IdentifyingNumber, Name, Vendor, Version, Caption | Sort-Object Name | Out-File $filename -append
+    $GetPackagesPrograms | Select-Object Name, Version, Source, ProviderName | Sort-Object Name | Out-File $filename -append
+    Upload-File -FilePath "C:\.texomans\SyncroRMM\Installed Programs.txt"
+    Remove-Item "C:\.texomans\SyncroRMM\Installed Programs.txt"
+}
+Exit 0
