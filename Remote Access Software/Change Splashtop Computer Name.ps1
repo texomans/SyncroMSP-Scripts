f64817209c0c08f1b0a9f4bb81b151c16753c0d6
Josh at Texoma Network Solutions <45087983+texomans@users.noreply.github.com>
2022-03-02 14:36:59 -0600

    Create Change Splashtop Computer Name.ps1

---
+++ Remote Access Software/Change Splashtop Computer Name.ps1
@@ -0,0 +1,33 @@
+## Run as SYSTEM
+## Needed variable is $computername
+
+:CheckOS
+IF("%PROGRAMFILES(X86)%")
+{
+    GOTO 64BIT
+} 
+    else 
+{
+    GOTO 32BIT
+}
+
+# IF EXIST "%PROGRAMFILES(X86)%" (GOTO 64BIT) ELSE (GOTO 32BIT)
+
+
+:32BIT
+
+reg add "HKLM\SOFTWARE\Splashtop Inc.\Splashtop Remote Server" /f /v CloudComputerName /t REG_SZ /d $computername
+
+GOTO END
+
+
+:64BIT
+
+reg add "HKLM\SOFTWARE\WOW6432Node\Splashtop Inc.\Splashtop Remote Server" /f /v CloudComputerName /t REG_SZ /d $computername
+
+GOTO END
+
+
+:END
+net stop splashtopremoteservice
+net start splashtopremoteservice
