6da2fafb4fcfcf3beea415af7a75b83a088c4dbc
Josh at Texoma Network Solutions <45087983+texomans@users.noreply.github.com>
2022-03-02 20:14:31 -0600

    Update Change Windows Hello Required PIN Length.ps1

--- Users & Groups/Change Windows Hello Required PIN Length.ps1
+++ Users & Groups/Change Windows Hello Required PIN Length.ps1
@@ -1,4 +1,5 @@
-## Run as SYSTEM
-## VAriable needed: PIN_Length
+## Run as: SYSTEM
+## Max Script Time: 2 Minutes
+## Variable Needed: $PIN_Length
 
 reg add "HKLM\SOFTWARE\Policies\Microsoft\PassportForWork\PINComplexity" /V MinimumPINLength /T REG_dWORD /D $PIN_Length /F
