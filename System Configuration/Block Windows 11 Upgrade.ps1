1e5afbd625a19326474fd2a4a20ecd58965b8cb7
Josh at Texoma Network Solutions <45087983+texomans@users.noreply.github.com>
2022-03-02 20:02:15 -0600

    Update Block Windows 11 Upgrade.ps1

--- System Configuration/Block Windows 11 Upgrade.ps1
+++ System Configuration/Block Windows 11 Upgrade.ps1
@@ -1,5 +1,8 @@
 ## Run as SYSTEM
 ## 1 Variable is required. Add $TargetVersion as the limit of upgrades. We have a dropdown with both "21H1" and "21H2" depending on the client.
+## This script blocks the upgrade to Windows 11 by setting the maximum version of Windows 10 to the latest Windows 10 update.
+## This prevents Windows Update from offering a Windows 11 upgrade.
+
 
 reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate /f /v TargetReleaseVersion /t REG_DWORD /d 1
 reg add HKLM\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate /f /v TargetReleaseVersionInfo /t REG_SZ /d $TargetVersion
