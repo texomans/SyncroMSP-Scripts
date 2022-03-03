db85588e07985fc67055675ebb84d3d4ebd0a688
Josh at Texoma Network Solutions <45087983+texomans@users.noreply.github.com>
2022-03-02 19:56:37 -0600

    Update Change Syncro Windows Service Description.bat

--- SyncroRMM/Change Syncro Windows Service Description.bat
+++ SyncroRMM/Change Syncro Windows Service Description.bat
@@ -1,4 +1,5 @@
-## Run as SYSTEM
+REM Run as: SYSTEM
+REM Max Script Time: 2 Minutes
 
 sc description Syncro "Monitoring Agent provided by YOUR_COMPANY_NAME"
 sc description SyncroLive "Monitoring Agent provided by YOUR_COMPANY_NAME"
